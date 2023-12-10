08d46426dc0649d25184e7fb76a1d70044fd4303
Danny Suggi Saputra <dannysaputra3003@gmail.com>
2023-12-10 11:35:19 +0700

    feat: movie details by id

--- src/main/java/projectuas/streamingPlatform/controller/MovieController.java
+++ src/main/java/projectuas/streamingPlatform/controller/MovieController.java
@@ -1,8 +1,14 @@
 package projectuas.streamingPlatform.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import projectuas.streamingPlatform.data.entity.Movie;
 import projectuas.streamingPlatform.service.MovieService;
 
 @Controller
@@ -14,8 +20,26 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @GetMapping("/movie-details")
-    public String movieDetails() {
+    @GetMapping("/movie-form")
+    public String showMovieForm(Model model){
+        Movie movie = new Movie();
+        model.addAttribute("movies", movie);
+        return "movie-form";
+    }
+
+    @PostMapping("/movie-form/save")
+    public String addMovie(@Valid @ModelAttribute("movies") Movie movie,
+                           BindingResult result,
+                           Model model) {
+        movieService.pushMovie(movie);
+        return "redirect:/movie";
+    }
+
+    @GetMapping("/movie-details/{id}")
+    public String movieDetails(@PathVariable("id") Long movieId, Model model) {
+        Movie movie = movieService.getMovieById(movieId);
+        model.addAttribute("movie", movie);
+        System.out.println(movie);
         return "movie-details";
     }
 
