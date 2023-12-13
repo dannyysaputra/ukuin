5485af96ca929f1290a6fbf976d2172b2968fead
Danny Suggi Saputra <dannysaputra3003@gmail.com>
2023-12-13 19:46:41 +0700

    Role entity

--- src/main/java/projectuas/streamingPlatform/config/CustomUser.java
+++ src/main/java/projectuas/streamingPlatform/config/CustomUser.java
@@ -3,13 +3,18 @@ package projectuas.streamingPlatform.config;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import projectuas.streamingPlatform.data.entity.Role;
 import projectuas.streamingPlatform.data.entity.User;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CustomUser implements UserDetails {
     private User user;
+    private List<Role> roles = new ArrayList<>();
 
     public CustomUser(User user) {
         super();
@@ -18,10 +23,13 @@ public class CustomUser implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+    }
 
-        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
-
-        return Arrays.asList(authority);
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
     }
 
     @Override
--- src/main/java/projectuas/streamingPlatform/service/impl/UserServiceImpl.java
+++ src/main/java/projectuas/streamingPlatform/service/impl/UserServiceImpl.java
@@ -2,11 +2,14 @@ package projectuas.streamingPlatform.service.impl;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import projectuas.streamingPlatform.data.entity.Role;
+import projectuas.streamingPlatform.data.repository.RoleRepository;
 import projectuas.streamingPlatform.dto.UserDto;
 import projectuas.streamingPlatform.data.entity.User;
 import projectuas.streamingPlatform.data.repository.UserRepository;
 import projectuas.streamingPlatform.service.UserService;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,13 +17,15 @@ import java.util.stream.Collectors;
 public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
+    private RoleRepository roleRepository;
     private UserService userService;
     private PasswordEncoder passwordEncoder;
 
     public UserServiceImpl(UserRepository userRepository,
-                           PasswordEncoder passwordEncoder) {
+                           PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
     }
 
     @Override
@@ -29,16 +34,23 @@ public class UserServiceImpl implements UserService {
         user.setName(userDto.getFirstName() + " " + userDto.getLastName());
         user.setUsername(userDto.getUsername());
         user.setEmail(userDto.getEmail());
-        user.setRole("ROLE_USER");
         user.setIsPremium(false);
-        // encrypt the password using spring security
         user.setPassword(passwordEncoder.encode(userDto.getPassword()));
 
-//        if(role == null){
-//            role = checkRoleExist();
-//        }
-//        user.setRoles(Arrays.asList(role));
+        Role role = roleRepository.findByName("ROLE_ADMIN");
+
+        if(role == null){
+            role = checkRoleExist();
+        }
+            user.setRoles(Arrays.asList(role));
+
         userRepository.save(user);
+    }
+
+    private Role checkRoleExist(){
+        Role role = new Role();
+        role.setName("ROLE_ADMIN");
+        return roleRepository.save(role);
     }
 
     @Override
